4987485564f45970ed5d85e42c36b30dfc3bf310
annaslipstick <mandowm@hotmail.com>
2022-05-27 11:29:24 +0200

    Added wincons for all situations, improved computer random function and draw function

--- index.js
+++ index.js
@@ -8,15 +8,36 @@ const pHasChosen = "Player has chosen:";
 var rand = choice[(Math.random() * choice.length) | 0]
 console.log(ComputerChoice, rand);
 
+
+
 //Create function for player, let player pick an alternative from choice array.
 var playerChoice = prompt("Please choose Rock, Paper or Scissors");
+
+
+//Compares the results of computer and players
 console.log(pHasChosen, playerChoice); 
-if (playerChoice.toLowerCase() && rand.toLowerCase() == "rock" || "paper" ||"scissors" ){ //reads the player choice in lowercase, no matter the input.
+if (playerChoice.toLowerCase() === rand.toLowerCase()){ //reads the player and computer choice in lowercase, no matter the input. Compares them to each other
     console.log("It's a draw!");
-}
-    else {
-        console.log("Upsie");
+}//From here on out, choose a winner. VERY inefficient code.
+    else if (playerChoice.toLowerCase() === "rock" && rand.toLowerCase() === "scissors"){ 
+        console.log("Player has won!");
     }
-
-
-    //computerOpp.toLowerCase
+    else if (playerChoice.toLowerCase() === "scissor" && rand.toLowerCase() === "paper"){ 
+        console.log("Player has won!");
+        
+    }
+    else if (playerChoice.toLowerCase() === "paper" && rand.toLowerCase() === "rock"){ 
+        console.log("Player has won!");
+        // Computer wins
+    }    else if (playerChoice.toLowerCase() === "paper" && rand.toLowerCase() === "scissors"){ 
+        console.log("Computer has won!");
+    }
+    else if (playerChoice.toLowerCase() === "rock" && rand.toLowerCase() === "paper"){ 
+        console.log("Computer has won!");
+        
+    }else if (playerChoice.toLowerCase() === "scissors" && rand.toLowerCase() === "rock"){ 
+        console.log("Computer has won!");
+        
+    }
+     else   console.log("Please choose a valid option");{
+    }
