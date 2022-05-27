e562e1704248bd31488164c5fbf95e423fa0f40d
annaslipstick <mandowm@hotmail.com>
2022-05-27 10:04:43 +0200

    added ComputerChoice and printed the list for player

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //Create a variable for rock/paper/scissor
 const choice = ["Rock", "Paper", "Scissors"];
+const ComputerChoice = "Computer has chosen:";
 
 //Function for computer opponent
 const computerOpp = Math.floor(Math.random() * choice.length);
-console.log(computerOpp, choice[computerOpp]);
+console.log( ComputerChoice, computerOpp, choice[computerOpp]);
 
 //Create function for player
+console.log(choice)
