de52fc66ca24d45307fca65a62fa75b75235a326
annaslipstick <mandowm@hotmail.com>
2022-05-27 10:22:04 +0200

    Added player choice and if statement

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 //Create a variable for rock/paper/scissor
 const choice = ["Rock", "Paper", "Scissors"];
 const ComputerChoice = "Computer has chosen:";
+const pHasChosen = "Player has chosen:";
 
 //Function for computer opponent
 const computerOpp = Math.floor(Math.random() * choice.length);
 console.log(ComputerChoice, computerOpp, choice[computerOpp]);
 
 //Create function for player, let player pick an alternative from choice array.
-const playerChoice = prompt("Please choose Rock, Paper or Scissors")
-console.log(playerChoice)
+var playerChoice = prompt("Please choose Rock, Paper or Scissors");
+console.log(pHasChosen, playerChoice);
+if (playerChoice == "Rock"){
+    console.log("You have chosen Rock");
+}
+    else {
+        console.log("Upsie");
+    }
