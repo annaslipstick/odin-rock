b15e5623cfdda00956ce94efa07acd28feb2c125
annaslipstick <mandowm@hotmail.com>
2022-05-27 11:07:16 +0200

    Added a draw function to the game

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 //Create a variable for rock/paper/scissor
-const choice = ["Rock", "Paper", "Scissors"];
+var choice = ["Rock", "Paper", "Scissors"];
 const ComputerChoice = "Computer has chosen:";
 const pHasChosen = "Player has chosen:";
 
+
 //Function for computer opponent
-const computerOpp = Math.floor(Math.random() * choice.length);
-console.log(ComputerChoice, computerOpp, choice[computerOpp]);
+var rand = choice[(Math.random() * choice.length) | 0]
+console.log(ComputerChoice, rand);
 
 //Create function for player, let player pick an alternative from choice array.
 var playerChoice = prompt("Please choose Rock, Paper or Scissors");
-console.log(pHasChosen, playerChoice);
-if (playerChoice.toLowerCase() == "rock"){
-    console.log("You have chosen Rock");
+console.log(pHasChosen, playerChoice); 
+if (playerChoice.toLowerCase() && rand.toLowerCase() == "rock" || "paper" ||"scissors" ){ //reads the player choice in lowercase, no matter the input.
+    console.log("It's a draw!");
 }
     else {
         console.log("Upsie");
     }
+
+
+    //computerOpp.toLowerCase
