0b0c3f55a584a156f6ae1bd262dcc1dcdf60a32f
annaslipstick <mandowm@hotmail.com>
2022-08-08 11:56:02 +0200

    solved the if-statement and the while loop, started the process with scoreboard

--- func.js
+++ func.js
@@ -1,4 +1,8 @@
 
+
+let i = 0
+while (i < 5) { 
+
 let playerSelection = "just a value"
 let replayInput = "just a value"
 let choice = ["Rock", "Paper", "Scissors"];
@@ -14,6 +18,8 @@ function playerPlay(playerSelection){
 }
 
 let pInput = playerPlay().toLowerCase();
+let computerScore = 0
+let playerScore = 0 
 
 
 
@@ -21,42 +27,83 @@ function playRound() {
 
     if (pInput == computerSelection){ 
         console.log("It's a draw!");
+        i++;
+        console.log("Computer's score = " + computerScore);
+        console.log("Player's score = " + playerScore);
     }
     else if (pInput == "scissors" && computerSelection == "paper") {
         console.log("Player win!");
+        i++;
+        console.log("Computer's score = " + computerScore);
+        console.log("Player's score = " + playerScore);
     }
     else if (pInput == "rock" && computerSelection == "scissors") {
         console.log("Player win!");
+        i++;
+        console.log("Computer's score = " + computerScore);
+        console.log("Player's score = " + playerScore);
     }
     else if (pInput == "paper" && computerSelection == "rock") {
         console.log("Player win!");
+        i++;
+        console.log("Computer's score = " + computerScore);
+        console.log("Player's score = " + playerScore);
     }
     else if (pInput == "paper" && computerSelection == "scissors") {
         console.log("Computer win!");
+        i++;
+        console.log("Computer's score = " + computerScore);
+        console.log("Player's score = " + playerScore);
     }
     else if (pInput == "rock" && computerSelection == "paper") {
         console.log("Computer win!");
+        i++;
+        console.log("Computer's score = " + computerScore);
+        console.log("Player's score = " + playerScore);
     }
     else if (pInput == "scissors" && computerSelection == "rock") {
         console.log("Computer win!");
+        i++;
+        console.log("Computer's score = " + computerScore);
+        console.log("Player's score = " + playerScore);
     }
     else {
-        console.log("It's a me, mario");
+        console.log("You have chosen an invalid option");
+        console.log("Computer's score = " + computerScore);
+        console.log("Player's score = " + playerScore);
+        i++;
     } 
   }
   
  
-  function playAgain(replayInput) {
-    replayInput = prompt("Do you want to play again? Write 'yes' or 'no'").toLowerCase();
-    if (replayInput = "yes") {
-    return(pInput)
-  }
-    else {
-        console.log("How rude of you!")
+  
+playRound()
+replayInput = prompt("Do you want to play again? Write 'yes' or 'no'").toLowerCase();
+
+if (replayInput == "yes") {
+        console.log("How kind of you! \n This is a best of 5!");
+        console.log("Amount of rounds played: " + i);
+        console.log("Computer's score = " + computerScore);
+        console.log("Player's score = " + playerScore);
+    }
+else if  (replayInput == "no") {
+        console.log("How rude of you!");
+        console.log("Amount of rounds played: " + i);
+        i++;
+        i++;
+        i++;
+        i++;
+        i++;
         } 
-  }
+else {
+    console.log("Invalid alternative, lets play again!");
+    console.log("Amount of rounds played: " + i);
+}
+}
 
-let playAgainDecision = playAgain().toLowerCase
+console.log("Five rounds has now been played \n The game is now over, thank you for participating!");
 
-playRound(pInput)
 
+
+
+//let playAgainDecision = playAgain().toLowerCase();
