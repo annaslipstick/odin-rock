95f9acff05154bd03cc6bec9fe783dfe94071063
annaslipstick <mandowm@hotmail.com>
2022-08-08 12:44:44 +0200

    completed scoreboard, finalized the game according to instructions

--- func.js
+++ func.js
@@ -1,13 +1,16 @@
 
-
+let computerScore = 0;
+let playerScore = 0 ;
+let winner;
 let i = 0
+
 while (i < 5) { 
 
 let playerSelection = "just a value"
 let replayInput = "just a value"
 let choice = ["Rock", "Paper", "Scissors"];
 let computerSelection = choice[(Math.random() * choice.length) | 0].toLowerCase();
-console.log("Computer chose " + computerSelection);
+
 
 
 
@@ -18,8 +21,7 @@ function playerPlay(playerSelection){
 }
 
 let pInput = playerPlay().toLowerCase();
-let computerScore = 0
-let playerScore = 0 
+console.log("Computer chose " + computerSelection);
 
 
 
@@ -27,66 +29,79 @@ function playRound() {
 
     if (pInput == computerSelection){ 
         console.log("It's a draw!");
+        winner = "It's a draw!";
         i++;
-        console.log("Computer's score = " + computerScore);
-        console.log("Player's score = " + playerScore);
+        return winner;
     }
     else if (pInput == "scissors" && computerSelection == "paper") {
         console.log("Player win!");
+        winner = "Player win!";
         i++;
-        console.log("Computer's score = " + computerScore);
-        console.log("Player's score = " + playerScore);
+        return   winner;
     }
     else if (pInput == "rock" && computerSelection == "scissors") {
         console.log("Player win!");
+        winner = "Player win!";
         i++;
-        console.log("Computer's score = " + computerScore);
-        console.log("Player's score = " + playerScore);
+        return winner;
     }
     else if (pInput == "paper" && computerSelection == "rock") {
+        winner = "Player win!";
         console.log("Player win!");
         i++;
-        console.log("Computer's score = " + computerScore);
-        console.log("Player's score = " + playerScore);
+        return winner;
     }
     else if (pInput == "paper" && computerSelection == "scissors") {
         console.log("Computer win!");
+        winner = "Computer win!";
         i++;
-        console.log("Computer's score = " + computerScore);
-        console.log("Player's score = " + playerScore);
+        return winner;
     }
     else if (pInput == "rock" && computerSelection == "paper") {
         console.log("Computer win!");
+        winner = "Computer win!";
         i++;
-        console.log("Computer's score = " + computerScore);
-        console.log("Player's score = " + playerScore);
+        return winner;
     }
     else if (pInput == "scissors" && computerSelection == "rock") {
         console.log("Computer win!");
+        winner = "Computer win!";
         i++;
-        console.log("Computer's score = " + computerScore);
-        console.log("Player's score = " + playerScore);
+        return winner;
     }
     else {
         console.log("You have chosen an invalid option");
-        console.log("Computer's score = " + computerScore);
-        console.log("Player's score = " + playerScore);
+        winner = "Computer win!";
         i++;
+        return winner;
     } 
   }
   
  
   
 playRound()
+
+if (winner == "Player win!") {
+    playerScore++;
+}
+else if (winner == "It's a draw!") {
+    
+}
+else {
+    computerScore++;
+}
+
+console.log("Computer's score = " + computerScore);
+console.log("Player's score = " + playerScore);
+
+
 replayInput = prompt("Do you want to play again? Write 'yes' or 'no'").toLowerCase();
 
-if (replayInput == "yes") {
+if (replayInput == "yes" && i < 5) {
         console.log("How kind of you! \n This is a best of 5!");
         console.log("Amount of rounds played: " + i);
-        console.log("Computer's score = " + computerScore);
-        console.log("Player's score = " + playerScore);
     }
-else if  (replayInput == "no") {
+else if  (replayInput == "no" && i < 5) {
         console.log("How rude of you!");
         console.log("Amount of rounds played: " + i);
         i++;
@@ -95,14 +110,19 @@ else if  (replayInput == "no") {
         i++;
         i++;
         } 
-else {
+else if (i < 5) {
     console.log("Invalid alternative, lets play again!");
     console.log("Amount of rounds played: " + i);
+}
+else {
+    console.log("Computer's score = " + computerScore);
+    console.log("Player's score = " + playerScore);
 }
 }
 
 console.log("Five rounds has now been played \n The game is now over, thank you for participating!");
-
+console.log("Computer's score = " + computerScore);
+console.log("Player's score = " + playerScore);
 
 
 
